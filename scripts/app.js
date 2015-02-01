9ca2d22a9478dd973c89e9492b564c0a35b544cb
Alfred Ababio <aababio75@gmail.com>
2015-01-31 22:21:38 -0800

    Configured theme and definited titles

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,24 @@
 
 var app = angular.module('alfredMaterial', ['ngMaterial']);
 
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+  .primaryPalette('blue')
+  .accentPalette('amber');
+});
+
 app.controller('AppCtrl', ['$scope', function($scope){
-  
+ 
+
+
+  $scope.subTitles = [{
+    name : 'Developer',
+    icon : 'keyboard'
+  }, {
+    name : 'Comic Nerd',
+    icon : 'book'
+  }, {
+    name : 'Longboarder',
+    icon : 'helmet'
+  }];
 }]);
