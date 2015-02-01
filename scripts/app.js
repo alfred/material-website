b6cf847bd8b2b9feb912f9f3abbff6df20183543
Alfred Ababio <aababio75@gmail.com>
2015-01-31 20:31:51 -0800

    Set a base

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,7 @@
-var app;
-aboutPage = angular.module("aboutPage", []);
+'use strict'
+
+var app = angular.module('alfredMaterial', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', function($scope){
+  
+}]);
