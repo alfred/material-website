36253d83a922962a25303a74d22a17d90d82eb2e
Alfred Ababio <aababio75@gmail.com>
2015-03-29 16:25:19 -0700

    Added nodemon task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'bin/www'
+  , ext: 'js hbs'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
 
 gulp.task('watch', function() {
   // Watch backend js and restart server
@@ -15,4 +25,11 @@ gulp.task('assets', function() {
   // minify js
   // watch sass and output css
   // 
+});
+
+gulp.task('deploy', function() {
+  nodemon({
+    script: 'bin/www'
+    
+  })
 });
