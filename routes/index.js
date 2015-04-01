04c2719a22be9da83d566e0075d6b86d231c473f
Alfred Ababio <aababio75@gmail.com>
2015-03-31 21:20:03 -0700

    Changed root route to wildcard

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var hbs = require('express-handlebars');
 var router = express.Router();
 
-/* GET home page. */
+router.get('*', function(req, res, next) {
+  res.render('home');
+});
+
 router.get('/', function(req, res, next) {
   res.render('home');
 });
