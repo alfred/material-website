d872c2bae4f1d1a0076218a76252bf5b30caa1f7
Alfred Ababio <aababio75@gmail.com>
2015-04-25 21:48:43 -0700

    Started figuring my shit out

--- views/projects/projects.js
+++ views/projects/projects.js
@@ -1,3 +1,54 @@
 app.controller('ProjectsCtrl', function($scope, $timeout, $mdSidenav, $log) {
 
+  var allProjects = {
+    'md-site': {
+      "name": "Alfred Material",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    },
+    'ef': {
+      "name": "Turbotax Exemption Check",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    },
+    'mean': {
+      "name": "MEAN Skeleton",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    },
+    'old-site': {
+      "name": "Old Website",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    },
+    'moderator': {
+      "name": "Cengage Moderator",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    },
+    'css-logos': {
+      "name": "CSS Logos",
+      "screenshot": '',
+      "description": '',
+      "repository": '',
+      "tech": []
+    }
+  };
+
+
+  $scope.makeActivePreview = function(newActiveName) {
+
+  };
+
+  $scope.activePreview = allProjects['md-site'];
 });
