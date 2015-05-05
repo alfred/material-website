1e73721b984a172e9bc00580dda957f894dc2b24
Alfred Ababio <aababio75@gmail.com>
2015-05-04 21:24:09 -0700

    Wrote a TODO for this in code, dope

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,25 @@ var http = require('http');
 var cheerio = require('cheerio'),
   request = require('request');
 
-// Will scrape motherfuckers.
+/**
+  Get date, parse into url params
+  Check and save total number of items
+  New variable issuesSeen counter
+  New variable pullList
+
+  [START LOOP]
+  Get TRs
+  Filter by those who have #synopsis child
+  Push JSON {title, imageUrl}
+  Save image to DB
+  Add 20 to issuesSeen
+  Change param in url
+  [STOP LOOP when issuesSeen >= totalNumber]
+
+  send pullList to front end
+  */
 router.get('/scrape', function(req, res, next) {
-  url = 'https://pulllist.comixology.com/dc_comics/2015/05/06'; // Date needs to be a param
+  url = 'https://pulllist.comixology.com/dc_comics/2015/05/06?start=0'; // Date needs to be a param
 
   request(url, function(err, resp, html) {
     if (err) {
@@ -16,13 +32,13 @@ router.get('/scrape', function(req, res, next) {
     var $ = cheerio.load(html);
 
     if ($('#listings').text() === "There are no items for this week.") {
-      res.send([]);
-      return;
-    }
 
+    }
+    var totalIssues = Integer.parseInt($('#results > div > div:nth-child(2) > strong:nth-child(2)').text());
+    var 
     var $rows = $('tr');
     $rows.filter(function(index, element) {
-      return ($(this).find('#synopsis').length);
+      $(this).find('#synopsis');
     });
   });
 });
