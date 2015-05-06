840053fb2390177ef7a40dce1657f18489b8e40b
Alfred Ababio <aababio75@gmail.com>
2015-05-05 21:15:24 -0700

    Still have some building to do on this, doesn't loop through all yet

--- routes/api.js
+++ routes/api.js
@@ -22,25 +22,44 @@ var cheerio = require('cheerio'),
   send pullList to front end
   */
 router.get('/scrape', function(req, res, next) {
-  url = 'https://pulllist.comixology.com/dc_comics/2015/05/06?start=0'; // Date needs to be a param
+  var date = new Date();
+  var month = date.getMonth() + 1, day = date.getDate(), year = date.getFullYear();
+  var url = 'https://pulllist.comixology.com/dc_comics/';
+  var issuesSeen = 0;
+  var queryString = String(year) + '/' + String(month) + '/30' + String(day) + '?start=' + issuesSeen;
+  var totalIssues = 0, pullList = [];
 
-  request(url, function(err, resp, html) {
+  console.log('About to make the request');
+  request.get(url, function(err, resp, html) {
     if (err) {
-      console.log('[ERROR] Could not scrape, ' + JSON.stringify(err));
+      console.error('[ERROR] Could not scrape, ' + JSON.stringify(err));
     }
-
     var $ = cheerio.load(html);
 
     if ($('#listings').text() === "There are no items for this week.") {
-
+      totalIssues = -1;
+      console.log('[INFO] No listings on this page, exiting loop with: ' + issuesSeen + ' issues this week.');
     }
-    var totalIssues = Integer.parseInt($('#results > div > div:nth-child(2) > strong:nth-child(2)').text());
-    var 
+    var totalIssues = parseInt($('#results > div > div:nth-child(2) > strong:nth-child(2)').text());
     var $rows = $('tr');
-    $rows.filter(function(index, element) {
-      $(this).find('#synopsis');
+    $rows.each(function(index , element) {
+      title = $(this).find('#synopsis').find('#title').find('a').text();
+      process.stdout.write(title + '\n');
+      imageUrl = $(this).find('#image').find('a > img').attr('src');
+      process.stdout.write(imageUrl + '\n');
+
+      if (title && validateImageURL(imageUrl, year)) {
+        pullList.push({
+          'title' : title,
+          'imageUrl' : imageUrl
+        });
+      }
     });
+    issuesSeen += 20;
+      // End the request down here right
+    res.send(pullList);
   });
+  
 });
 
 // Route that will function and get all of my relevant titles
@@ -49,4 +68,7 @@ router.get('52', function(req, res, next) {
 
 });
 
+function validateImageURL(url, year) {
+  return url.indexOf('/' + year + '/') !== 1;
+};
 module.exports = router;
