954ca9baa72d19eb62666ec4eda23111e2399c4d
Alfred Ababio <aababio75@gmail.com>
2015-05-30 01:42:26 -0700

    Separated the things into arrays instead of separate p tags for ng-repeat

--- views/home/home.js
+++ views/home/home.js
@@ -1,23 +1,22 @@
 app.controller('HomeCtrl', function($scope) {
   $scope.hobbies = [{
     name: "Longboarder",
-    desc: "I learned to ride about a year ago, and it might have been one of the" +
-          "  best things I\'ve done in awhile. Its a really fun way to get" +
-          " around and an even better way to stay active. I also managed to" +
-          " design and paint my own decal onto my board!"
+    desc: ["I learned to ride about a year ago, and learning might have been one of the" +
+          "  best things I\'ve ever done. It\'s a really fun way to get" +
+          " around and an even better way to stay active.",
+          " I even managed to design a decal and paint it onto my board!"]
   },{
     name: "Comic Reader",
-    desc: "I\'m a huge DC comics fan. I can appreciate the amount of work that goes" + 
-          " into building not only a universe, but a multiverse. There are so many moving parts to factor in" +
-          " and its amazing to see how authors can stick to canon, but also continue to innovate " +
-          "the story and present their fans with new ideas every issue.",
+    desc: ["I\'m a huge DC comics fan. I can appreciate the amount of work that goes into building a multiverse.",
+          "There are so many moving parts to factor in and it\'s amazing to see how " + 
+          " authors can continue to innovate the characters and present their fans with new topics to think about every issue."],
     link: "#comics"
   },{
     name: "Developer",
-    desc: "I\'m a full-stack web developer. Javascript is my weapon of choice. " +
+    desc: ["I\'m a full-stack web developer who happens to write lots of Javascript. ",
           "I started off building apps with Ruby on Rails and have moved on to" +
-          "really enjoying Node and Express. What you\'re looking at right now was actually built with " +
-          "Node and Express, and a generous amount of Angular. Check out my other work here",
+          " Node and Express. What you\'re looking at right now was actually built with " +
+          "Node and Express, and a generous amount of Angular. Check out my other work here"],
     link: ""
   }];
 
@@ -41,8 +40,9 @@ app.controller('HomeCtrl', function($scope) {
 
   $scope.aboutText = {          
     intro: "Hi, I\'m Alfred.",     
-    body: "I\'m a Computer Science student at Northeastern, an amateur pool (pocket billiards) player, " + 
-          "and a firm believer that anyone can learn to do anything by visiting the" +
-          "appropriate subreddit. \n My friends think I\'m a pretty cool guy, though they\'ll deny it after reading this."
+    body: [
+      "I\'m a Computer Science student at Northeastern, an amateur pool (pocket billiards) player, " + 
+      "and a firm believer that anyone can learn to do anything by Googling it long enough.",
+      "Also, my friends think I\'m a pretty cool guy (even though they\'ll deny it after reading this.)"]
   };  
 });
