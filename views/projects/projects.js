710f45c50f9b0ae33052599659460c7c40963195
Alfred Ababio <aababio75@gmail.com>
2015-06-16 21:05:39 -0700

    Commented out Exemption Check because I never asked if I could use it

--- views/projects/projects.js
+++ views/projects/projects.js
@@ -10,15 +10,15 @@ app.controller('ProjectsCtrl', function($scope, $timeout, $mdSidenav, $log) {
       "tech": [],
       "learned": ''
     },
-    'ef': {
-      "name": "TurboTax Exemption Check",
-      "screenshot": '/imgs/projects/tt-ef.png',
-      "description": '',
-      "repository": '',
-      "demoLink": 'https://turbotax.intuit.com/health-care/exemptions',
-      "tech": [],
-      "learned": ''
-    },
+    // 'ef': {
+    //   "name": "TurboTax Exemption Check",
+    //   "screenshot": '/imgs/projects/tt-ef.png',
+    //   "description": '',
+    //   "repository": '',
+    //   "demoLink": 'https://turbotax.intuit.com/health-care/exemptions',
+    //   "tech": [],
+    //   "learned": ''
+    // },
     'oldSite': {
       "name": "Old Website",
       "screenshot": '/imgs/projects/old-site.png',
@@ -70,9 +70,7 @@ app.directive('projImg', function(){
     return function(scope, element, attrs){
         attrs.$observe('projImg', function(value) {
             element.css({
-                'background-image': 'url(' + value +')',
-                'background-size' : 'cover'
-                // 'background-position' : 'center'
+                'background-image': 'url(' + value +')'
             });
         });
     };
