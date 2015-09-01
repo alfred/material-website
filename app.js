1ec16b492547d7c246e91fb4c56cad429adb8422
Alfred Ababio <aababio75@gmail.com>
2015-09-01 01:36:01 -0400

    Added gzip compression

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
+var compress = require('compression');
 var mongoose = require('mongoose');
 
 
@@ -21,6 +21,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(compress());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
